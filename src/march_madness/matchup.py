de47fe6e23a0ea66aa9f401da3c51d8b66ee1326
harrisonfloam <harrisonfloam@gmail.com>
2025-03-17 22:06:36 -0400

    rework game_id as a single integer

--- src/march_madness/matchup.py
+++ src/march_madness/matchup.py
@@ -17,11 +17,8 @@ NCAA_BRACKET_PROGRESSION = [
     6, 11, 3, 14,
     7, 10, 2, 15
 ]
-# Final Four region matchups
-NCAA_REGION_MATCHUPS = [
-    ("East", "West"),
-    ("South", "Midwest")
-]
+
+NCAA_REGION_ORDER = ["East", "West", "South", "Midwest"]
 
 def ncaa_initial_matchups(tournament: "Tournament") -> List[Matchup]:
     """
@@ -30,18 +27,13 @@ def ncaa_initial_matchups(tournament: "Tournament") -> List[Matchup]:
     In each region, the highest seed plays the lowest, second-highest plays second-lowest: 
     1 vs. 16, 2 vs. 15, etc.
     """
+    game_id = 0
     matchups = []
-    regions = set(team["region"] for team in tournament.teams.values())  # All original regions
-
-    for region in regions:
+    for region in NCAA_REGION_ORDER:
         teams_by_seed = {team["seed"]: team for team in tournament.teams.values() if team["region"] == region}
 
-        for game_number, (seed1, seed2) in enumerate(itertools.batched(NCAA_BRACKET_PROGRESSION, 2), start=1):
-            game_id = {
-                "round": 1,
-                "region": region,
-                "game": game_number
-            }
+        for (seed1, seed2) in itertools.batched(NCAA_BRACKET_PROGRESSION, 2):
+            game_id += 1
 
             matchups.append((teams_by_seed[seed1], teams_by_seed[seed2], game_id))  # Highest vs. Lowest
     return matchups
@@ -59,45 +51,34 @@ def ncaa_round_matchups(tournament: "Tournament") -> List[Matchup]:
     # Get winners from last round
     prev_round_games = [game for game in tournament.played_games if game.winner and game.round_number == tournament.current_round-1]
     num_winners = len(prev_round_games)
-    regions = set(team["region"] for team in tournament.teams.values())  # All original regions
+    num_regions = len(NCAA_REGION_ORDER)
+    game_id = int(sum(64/2**r for r in range(1, tournament.current_round)))
     matchups = []
 
     # Regional rounds
-    if num_winners > len(regions):
-        for region in regions:
+    if num_winners > num_regions:
+        for region in NCAA_REGION_ORDER:
             region_winners = [tournament.teams[game.winner] for game in prev_round_games if game.winner_details["region"] == region]
             
             # Iterate over chunks of the bracket
             # In round 1, winners must come from seeds {16, 1, 8, 9}...
-            for game_number, round_bracket in enumerate(itertools.batched(NCAA_BRACKET_PROGRESSION, 2**tournament.current_round), start=1):
+            for round_bracket in itertools.batched(NCAA_BRACKET_PROGRESSION, 2**tournament.current_round):
                 round_bracket_set = set(round_bracket)
                 matchup = tuple(team for team in region_winners if team["seed"] in round_bracket_set)
-                game_id = {
-                    "round": tournament.current_round,
-                    "region": region,
-                    "game": game_number
-                }
+                game_id += 1
                 matchups.append((*matchup, game_id))
                 
     # Final Four
-    elif num_winners == len(regions):
+    elif num_winners == num_regions:
         winners_by_region = {tournament.teams[game.winner]["region"]: tournament.teams[game.winner] for game in prev_round_games}
-        for region1, region2 in NCAA_REGION_MATCHUPS:
-            game_id = {
-                "round": tournament.current_round,
-                "region": f"{region1}-{region2}",
-                "game": 1
-            }
+        for region1, region2 in zip(NCAA_REGION_ORDER[:2], NCAA_REGION_ORDER[2:]):
+            game_id += 1
             matchups.append((winners_by_region[region1], winners_by_region[region2], game_id))
             
     # Championship
     else:
         winners = tuple(tournament.teams[game.winner] for game in prev_round_games)
-        game_id = {
-            "round": tournament.current_round,
-            "region": "championship",
-            "game": 1
-        }
+        game_id += 1
         matchups.append((*winners, game_id))
 
     return matchups
